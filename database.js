7dbcb83c87772d2bfe69ab1eeb4ef5e32bda97c2
daesung <dasung56@gmail.com>
2024-05-16 03:57:01 +0900

    env로 링크 숨기기

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { MongoClient} = require('mongodb');
 
-const url = 'mongodb+srv://daesung:ReJWrIXa1WEzyPBF@cluster0.v4siqil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const url = process.env.DB_URL
 let connectDB = new MongoClient(url).connect()
 
 module.exports = connectDB
